d700879afd94702ec939172f90a21f32ebb87a3e
Euan <euanwidjaja@yahoo.co.nz>
2024-01-17 21:53:41 +1300

    Implement basic hero screen

---
+++ app/page.tsx
@@ -0,0 +1,21 @@
+import NavigationBar from './ui/navbar/navbar'
+import styles from './ui/app.module.css'
+
+export default function Page() {
+  return (
+    <>
+      <NavigationBar />
+      {/* <div className="hero min-h-screen" style={{backgroundImage: 'url(https://daisyui.com/images/stock/photo-1507358522600-9f71e620c44e.jpg)'}}> */}
+      <div className={`hero min-h-screen ${styles.container}`}>
+        <div className="hero-overlay bg-opacity-60"></div>
+        <div className="hero-content text-center text-neutral-content">
+          <div className="max-w-md">
+            <h1 className="mb-5 text-5xl font-bold">Overseas Study Centre</h1>
+            <p className="mb-5">Provident cupiditate voluptatem et in. Quaerat fugiat ut assumenda excepturi exercitationem quasi. In deleniti eaque aut repudiandae et a id nisi.</p>
+            <button className="btn btn-primary">Get Started</button>
+          </div>
+        </div>
+      </div>
+    </>
+  )
+}
---
+++ app/ui/navbar/navbar.tsx
@@ -0,0 +1,22 @@
+import Link from 'next/link';
+import NavLinks from './navlinks';
+import React from 'react'
+
+const NavigationBar = () => {
+  return (
+    <div className="navbar bg-base-100 content-end">
+      <div className="flex-1">
+        <Link className="btn btn-ghost text-2xl" href="/">Overseas Study Centre</Link>
+      </div>
+      <div className="flex-none">
+        <ul className="menu menu-horizontal px-1">
+          <NavLinks />
+        </ul>
+      </div>
+    </div>
+  )
+}
+
+export default NavigationBar
+
+// NEXT STEPS: Follow https://nextjs.org/learn/dashboard-app/getting-started for folder structures etc
